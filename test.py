4949ef47a60e4f024dafdd1f3f3205ff19080295
Wang Kunpeng <1289706727@qq.com>
2026-04-15 16:49:58 +0800

    1

--- test.py
+++ test.py
@@ -11,7 +11,47 @@ from vllm_ascend.quantization.methods.base import (
 )
 
 
-class TestSchemeRegistry(TestBase):
+class TestRegisterScheme(TestBase):
+
+    def setUp(self):
+        super().setUp()
+        self._original_registry = _SCHEME_REGISTRY.copy()
+
+    RegisterScheme1 = register_scheme("TEST_QUANT_TYPE", "linear")(MockLinearScheme)
+    RegisterScheme2 = register_scheme("TEST_QUANT_TYPE", "moe")(MockMoEScheme)
+
+    def test_register_scheme_registers_class(self):
+        cls = get_scheme_class("TEST_QUANT_TYPE", "linear")
+        self.assertIs(cls, MockLinearScheme)
+
+    def test_register_scheme_registers_multiple_schemes(self):
+        linear_cls = get_scheme_class("TEST_QUANT_TYPE", "linear")
+        moe_cls = get_scheme_class("TEST_QUANT_TYPE", "moe")
+        self.assertIs(linear_cls, MockLinearScheme)
+        self.assertIs(moe_cls, MockMoEScheme)
+
+    def test_register_duplicate_scheme_raises_value_error(self):
+        with self.assertRaises(ValueError) as context:
+            register_scheme("TEST_QUANT_TYPE", "linear")(MockLinearScheme)
+        self.assertIn("already registered", str(context.exception))
+        self.assertIn("TEST_QUANT_TYPE/linear", str(context.exception))
+
+    def test_register_different_quant_types_same_layer_type(self):
+        @register_scheme("TEST_QUANT_TYPE_2", "linear")
+        class AnotherLinearScheme(AscendLinearScheme):
+            def get_weight(self, input_size, output_size, params_dtype):
+                return {}
+
+            def apply(self, layer, x, bias=None, tp_rank=0):
+                return x
+
+        cls1 = get_scheme_class("TEST_QUANT_TYPE", "linear")
+        cls2 = get_scheme_class("TEST_QUANT_TYPE_2", "linear")
+        self.assertIs(cls1, MockLinearScheme)
+        self.assertIs(cls2, AnotherLinearScheme)
+
+
+class TestGetSchemeClass(TestBase):
 
     def test_get_scheme_class_existing_linear(self):
         cls = get_scheme_class("W8A8_DYNAMIC", "linear")

eca9f94675f83b3f5dcde8efa381f80c07eaaf8e
Wang Kunpeng <1289706727@qq.com>
2026-04-16 09:34:04 +0800

    1

--- test.py
+++ test.py
@@ -13,42 +13,17 @@ from vllm_ascend.quantization.methods.base import (
 
 class TestRegisterScheme(TestBase):
 
-    def setUp(self):
-        super().setUp()
-        self._original_registry = _SCHEME_REGISTRY.copy()
-
-    RegisterScheme1 = register_scheme("TEST_QUANT_TYPE", "linear")(MockLinearScheme)
-    RegisterScheme2 = register_scheme("TEST_QUANT_TYPE", "moe")(MockMoEScheme)
-
-    def test_register_scheme_registers_class(self):
-        cls = get_scheme_class("TEST_QUANT_TYPE", "linear")
-        self.assertIs(cls, MockLinearScheme)
-
-    def test_register_scheme_registers_multiple_schemes(self):
-        linear_cls = get_scheme_class("TEST_QUANT_TYPE", "linear")
-        moe_cls = get_scheme_class("TEST_QUANT_TYPE", "moe")
-        self.assertIs(linear_cls, MockLinearScheme)
-        self.assertIs(moe_cls, MockMoEScheme)
-
-    def test_register_duplicate_scheme_raises_value_error(self):
-        with self.assertRaises(ValueError) as context:
-            register_scheme("TEST_QUANT_TYPE", "linear")(MockLinearScheme)
-        self.assertIn("already registered", str(context.exception))
-        self.assertIn("TEST_QUANT_TYPE/linear", str(context.exception))
-
-    def test_register_different_quant_types_same_layer_type(self):
-        @register_scheme("TEST_QUANT_TYPE_2", "linear")
-        class AnotherLinearScheme(AscendLinearScheme):
+    def test_register_scheme(self):
+        @register_scheme("TEST_QUANT_TYPE", "linear")
+        class TestLinearScheme(AscendLinearScheme):
             def get_weight(self, input_size, output_size, params_dtype):
                 return {}
 
             def apply(self, layer, x, bias=None, tp_rank=0):
                 return x
 
-        cls1 = get_scheme_class("TEST_QUANT_TYPE", "linear")
-        cls2 = get_scheme_class("TEST_QUANT_TYPE_2", "linear")
-        self.assertIs(cls1, MockLinearScheme)
-        self.assertIs(cls2, AnotherLinearScheme)
+        scheme_class = get_scheme_class("TEST_QUANT_TYPE", "linear")
+        self.assertIs(scheme_class, TestLinearScheme)
 
 
 class TestGetSchemeClass(TestBase):
@@ -97,15 +72,6 @@ class TestGetSchemeClass(TestBase):
                     issubclass(scheme_cls, AscendMoEScheme),
                     f"{scheme_cls.__name__} for {quant_type}/{layer_type} "
                     f"should be subclass of AscendMoEScheme",
-                )
-
-    def test_all_attention_schemes_subclass_ascend_attention_scheme(self):
-        for (quant_type, layer_type), scheme_cls in _SCHEME_REGISTRY.items():
-            if layer_type == "attention":
-                self.assertTrue(
-                    issubclass(scheme_cls, AscendAttentionScheme),
-                    f"{scheme_cls.__name__} for {quant_type}/{layer_type} "
-                    f"should be subclass of AscendAttentionScheme",
                 )
 
     def test_registry_not_empty(self):

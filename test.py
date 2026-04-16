53d12b1af9ffa6b843d738b13a38f02af2f3503a
Wang Kunpeng <1289706727@qq.com>
2026-04-16 14:38:02 +0800

    1

--- test.py
+++ test.py
@@ -1,84 +1,29 @@
 from tests.ut.base import TestBase
-from vllm_ascend.quantization.methods.registry import (
-    _SCHEME_REGISTRY,
-    register_scheme,
-    get_scheme_class,
-)
-from vllm_ascend.quantization.methods.base import (
-    AscendLinearScheme,
-    AscendAttentionScheme,
-    AscendMoEScheme,
-)
+from vllm_ascend.quantization.quant_type import QuantType
 
 
-class TestRegisterScheme(TestBase):
+class TestQuantType(TestBase):
 
-    def test_register_scheme(self):
-        @register_scheme("TEST_QUANT_TYPE", "linear")
-        class TestLinearScheme(AscendLinearScheme):
-            def get_weight(self, input_size, output_size, params_dtype):
-                return {}
+    def test_enum_values(self):
+        self.assertEqual(QuantType.NONE.value, 0)
+        self.assertEqual(QuantType.W8A8.value, 1)
+        self.assertEqual(QuantType.W4A8.value, 2)
+        self.assertEqual(QuantType.MXFP8.value, 3)
+        self.assertEqual(QuantType.W4A16.value, 4)
+        self.assertEqual(QuantType.MXFP4.value, 5)
 
-            def apply(self, layer, x, bias=None, tp_rank=0):
-                return x
+    def test_enum_members_count(self):
+        members = list(QuantType)
+        self.assertEqual(len(members), 6)
 
-        scheme_class = get_scheme_class("TEST_QUANT_TYPE", "linear")
-        self.assertIs(scheme_class, TestLinearScheme)
+    def test_enum_identity(self):
+        self.assertIs(QuantType.W8A8, QuantType.W8A8)
+        self.assertIsNot(QuantType.W8A8, QuantType.W4A8)
 
+    def test_enum_by_value(self):
+        self.assertEqual(QuantType(1), QuantType.W8A8)
+        self.assertEqual(QuantType(3), QuantType.MXFP8)
 
-class TestGetSchemeClass(TestBase):
-
-    def test_get_scheme_class_existing_linear(self):
-        cls = get_scheme_class("W8A8_DYNAMIC", "linear")
-        self.assertIsNotNone(cls)
-        self.assertTrue(issubclass(cls, AscendLinearScheme))
-
-    def test_get_scheme_class_existing_moe(self):
-        cls = get_scheme_class("W8A8_DYNAMIC", "moe")
-        self.assertIsNotNone(cls)
-        self.assertTrue(issubclass(cls, AscendMoEScheme))
-
-    def test_get_scheme_class_existing_attention(self):
-        cls = get_scheme_class("FAKQuant", "attention")
-        self.assertIsNotNone(cls)
-
-    def test_get_scheme_class_nonexistent(self):
-        cls = get_scheme_class("NONEXISTENT", "linear")
-        self.assertIsNone(cls)
-
-    def test_get_scheme_class_nonexistent_layer_type(self):
-        cls = get_scheme_class("W8A8_DYNAMIC", "nonexistent")
-        self.assertIsNone(cls)
-
-    def test_register_scheme_duplicate_raises(self):
-        with self.assertRaises(ValueError):
-            @register_scheme("W8A8_DYNAMIC", "linear")
-            class Duplicate:
-                pass
-
-    def test_all_linear_schemes_subclass_ascend_linear_scheme(self):
-        for (quant_type, layer_type), scheme_cls in _SCHEME_REGISTRY.items():
-            if layer_type == "linear":
-                self.assertTrue(
-                    issubclass(scheme_cls, AscendLinearScheme),
-                    f"{scheme_cls.__name__} for {quant_type}/{layer_type} "
-                    f"should be subclass of AscendLinearScheme",
-                )
-
-    def test_all_moe_schemes_subclass_ascend_moe_scheme(self):
-        for (quant_type, layer_type), scheme_cls in _SCHEME_REGISTRY.items():
-            if layer_type == "moe":
-                self.assertTrue(
-                    issubclass(scheme_cls, AscendMoEScheme),
-                    f"{scheme_cls.__name__} for {quant_type}/{layer_type} "
-                    f"should be subclass of AscendMoEScheme",
-                )
-
-    def test_registry_not_empty(self):
-        self.assertGreater(len(_SCHEME_REGISTRY), 0)
-
-    def test_registry_key_format(self):
-        for key in _SCHEME_REGISTRY.keys():
-            self.assertEqual(len(key), 2)
-            self.assertIsInstance(key[0], str)
-            self.assertIsInstance(key[1], str)
+    def test_enum_by_name(self):
+        self.assertEqual(QuantType["NONE"], QuantType.NONE)
+        self.assertEqual(QuantType["W4A16"], QuantType.W4A16)
